997d49c48bd138795bc62d18334c7baa463e9ad3
Rishika <rishika.umesh.kalyanappa@student.samk.fi>
2022-03-22 15:33:51 +0200

    made the bot to reply to chats

--- project/backend/bot.py
+++ project/backend/bot.py
@@ -1,6 +1,6 @@
 def bot_response(message):
-    if message=="hello": 
-        return "Hello human being"
+    if message in ("moi","moikka","hello") : #list of words that user can input 
+        return "Hello there human being"
     elif message=="goodbye": 
         return "Short circuit excecuted"
     elif message=="do you even work":
@@ -8,6 +8,6 @@ def bot_response(message):
     elif message.startswith("weather in"): #anything that starts with weather in, it will respond this
         return "I do not know the weather yet"
     elif "boring" in message: #is bring is there in the message, it will return back the message
-        return "Am i boring to you "
+        return "<h2>Am i boring to you</h2> " #implementing html factors in the chatbot 
     else:
         return "Cannot comprehend"
